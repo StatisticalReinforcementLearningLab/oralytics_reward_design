4202c251fc9353204966eea652880f79b0a9c93b
Anna Li <annajli@berkeley.edu>
2022-08-10 14:55:27 -0400

    adding metrics code and updating running experiments

--- code/run_rl_experiment.py
+++ code/run_rl_experiment.py
@@ -1,12 +1,13 @@
 from rl_experiments import *
 from absl import app
 from absl import flags
+import pickle
 
 # abseil ref: https://abseil.io/docs/python/guides/flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string('sim_env_type', None, 'input the simulation environment type')
-flags.DEFINE_string('algorithm_vals', None, 'input the RL algorithm candidate type')
-flags.DEFINE_integer('seed', None, 'seed for np.random.seed() to reproduce the trial')
+flags.DEFINE_string('algorithm_val_1', None, 'input the RL algorithm candidate value for var 1')
+flags.DEFINE_string('algorithm_val_2', None, 'input the RL algorithm candidate value for var 2')
 
 NUM_TRIAL_USERS = 72
 def get_user_list(study_idxs):
@@ -14,49 +15,54 @@ def get_user_list(study_idxs):
 
     return user_list
 
+MAX_SEED_VAL = 100
+
 # parses argv to access FLAGS
 def main(_argv):
-    # draw different users per trial
-    np.random.seed(FLAGS.seed)
-    print("SEED: ", FLAGS.seed)
     # denotes a weekly update schedule
     UPDATE_CADENCE = 13
-    STUDY_IDXS = np.random.choice(NUM_USERS, size=NUM_TRIAL_USERS)
-    print(STUDY_IDXS)
 
     ## HANDLING RL ALGORITHM CANDIDATE ##
-    cost_params = [int(x) for x in FLAGS.algorithm_vals.split('_')]
-    print("PROCESSED CANDIDATE VALS {}".format(candidate_type))
+    cost_params = [int(FLAGS.algorithm_val_1), int(FLAGS.algorithm_val_2)]
+    print("PROCESSED CANDIDATE VALS {}".format(cost_params))
     alg_candidate = BayesianLinearRegression(cost_params, UPDATE_CADENCE)
 
-    # get user ids corresponding to index
-    USERS_LIST = get_user_list(STUDY_IDXS)
-    print(USERS_LIST)
+    for current_seed in range(MAX_SEED_VAL):
+        # draw different users per trial
+        np.random.seed(current_seed)
+        print("SEED: ", current_seed)
 
-    ## HANDLING SIMULATION ENVIRONMENT ##
-    env_type = FLAGS.sim_env_type
-    if env_type == 'U_LOW':
-        environment_module = U_LOW(USERS_LIST)
-    elif env_type == 'U_MED':
-        environment_module = U_MED(USERS_LIST)
-    elif env_type == 'U_HIGH':
-        environment_module = U_HIGH(USERS_LIST)
-    else:
-        print("ERROR: NO ENV_TYPE FOUND - ", env_type)
+        STUDY_IDXS = np.random.choice(NUM_USERS, size=NUM_TRIAL_USERS)
+        print(STUDY_IDXS)
 
-    print("PROCESSED ENV_TYPE {}".format(env_type))
+        # get user ids corresponding to index
+        USERS_LIST = get_user_list(STUDY_IDXS)
+        print(USERS_LIST)
 
-    ## RUN EXPERIMENT ##
-    # Full Pooling with Incremental Recruitment
-    results = run_incremental_recruitment_exp(pre_process_users(USERS_LIST), alg_candidate, environment_module)
+        ## HANDLING SIMULATION ENVIRONMENT ##
+        env_type = FLAGS.sim_env_type
+        if env_type == 'U_LOW':
+            environment_module = U_LOW(USERS_LIST)
+        elif env_type == 'U_MED':
+            environment_module = U_MED(USERS_LIST)
+        elif env_type == 'U_HIGH':
+            environment_module = U_HIGH(USERS_LIST)
+        else:
+            print("ERROR: NO ENV_TYPE FOUND - ", env_type)
 
-    pickling_location = '/n/home02/atrella/processed_pickle_results/{}_{}_{}_processed_result.p'.format(FLAGS.algorithm_candidate, FLAGS.sim_env_type, FLAGS.seed)
+        print("PROCESSED ENV_TYPE {}".format(env_type))
 
-    ## results is a list of tuples where the first element of the tuple is user_id and the second element is a dictionary of values
-    print("TRIAL DONE, PICKLING NOW")
-    print("PICKLING TO: {}".format(pickling_location))
-    with open(pickling_location, 'wb') as f:
-        pickle.dump(results, f)
+        ## RUN EXPERIMENT ##
+        # Full Pooling with Incremental Recruitment
+        results = run_incremental_recruitment_exp(pre_process_users(USERS_LIST), alg_candidate, environment_module)
+
+        pickling_location = '/n/home02/atrella/reward_design/pickle_results/{}_{}_{}_result.p'.format(FLAGS.algorithm_candidate, FLAGS.sim_env_type, current_seed)
+
+        ## results is a list of tuples where the first element of the tuple is user_id and the second element is a dictionary of values
+        print("TRIAL DONE, PICKLING NOW")
+        print("PICKLING TO: {}".format(pickling_location))
+        with open(pickling_location, 'wb') as f:
+            pickle.dump(results, f)
 
 
 if __name__ == '__main__':
